f8e43591d15345e05fe2ac55059fb4bdf635220c
leonardofazolo <leonardogf.contato@gmail.com>
2024-06-18 15:33:01 -0300

    Update/restore packages
    Update/restore packages

--- FreightControlMaui/MauiProgram.cs
+++ FreightControlMaui/MauiProgram.cs
@@ -1,25 +1,33 @@
 ﻿using Microsoft.Extensions.Logging;
+using CommunityToolkit.Maui;
+using SkiaSharp.Views.Maui.Controls.Hosting;
+using Microcharts.Maui;
+using DevExpress.Maui;
 
 namespace FreightControlMaui;
-
 public static class MauiProgram
 {
-	public static MauiApp CreateMauiApp()
-	{
-		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .UseSkiaSharp()
+            .UseDevExpress()
+            .UseDevExpressCollectionView()
+            .UseDevExpressControls()
+            .UseDevExpressEditors()
+            .UseMicrocharts()
+            .UseMauiCommunityToolkit()
+            .ConfigureFonts(fonts =>
+        {
+            fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+            fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+        });
 
 #if DEBUG
-		builder.Logging.AddDebug();
+        builder.Logging.AddDebug();
 #endif
-
-		return builder.Build();
-	}
+        return builder.Build();
+    }
 }
-
