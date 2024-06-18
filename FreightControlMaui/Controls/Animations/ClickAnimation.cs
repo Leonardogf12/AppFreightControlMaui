546ec5293575b4b83d6eaed524740440ad8bb83c
leonardofazolo <leonardogf.contato@gmail.com>
2024-06-18 15:59:46 -0300

    Add new Folder and class
    Add Controls/Animation folder
    Add ClickAnimation class

---
+++ FreightControlMaui/Controls/Animations/ClickAnimation.cs
@@ -0,0 +1,27 @@
+﻿namespace FreightControlMaui.Controls.Animations
+{
+    public class ClickAnimation
+    {
+        const int DefaultScale = 1;
+        const double Scale = 0.95;
+        const uint DurationOfAnimantion = 100;
+
+        const double Fade = 0.5;
+        const uint Length = 100;
+        const int DefaultOpacit = 1;
+
+        public async Task SetScaleOnElement(View element, double scale = Scale, uint durationOfAnimation = DurationOfAnimantion)
+        {
+            await element.ScaleTo(scale, durationOfAnimation, Easing.Linear);
+            await element.ScaleTo(DefaultScale, durationOfAnimation, Easing.Linear);
+        }
+
+        public async Task SetFadeOnElement(View element, int delay = 100, double fade = Fade)
+        {
+            await element.FadeTo(fade, Length, Easing.SpringIn);
+            await Task.Delay(delay);
+            await element.FadeTo(DefaultOpacit, Length);
+        }
+    }
+}
+
