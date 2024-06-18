1c8d8ded577681ae8becc800d2a2081f59921b86
leonardofazolo <leonardogf.contato@gmail.com>
2024-06-18 15:36:02 -0300

    Add Folder Components
    Add folder Chart
    Add ChartStyleCustom class

---
+++ FreightControlMaui/Components/Chart/ChartStyleCustom.cs
@@ -0,0 +1,48 @@
+﻿using Microcharts;
+using SkiaSharp;
+
+namespace FreightControlMaui.Components.Chart
+{
+	public static class ChartStyleCustom
+	{
+        public static LineChart GetLineChartCustom(ChartEntry[] entries)
+        {
+            return new LineChart
+            {
+                Entries = entries,
+                IsAnimated = true,
+                LineMode = LineMode.Straight,
+                PointMode = PointMode.Circle,
+                LabelTextSize = 35,
+                PointSize = 20,
+                MaxValue = 100,
+                MinValue = 0,
+                Margin = 50,
+                LabelOrientation = Orientation.Horizontal,
+                ValueLabelOrientation = Orientation.Horizontal,
+                BackgroundColor = SKColor.Parse("#333850"),
+                LabelColor = SKColor.Parse("#FFFFFF"),
+                Typeface = SKTypeface.Default,
+            };
+        }
+
+        public static BarChart GetBarChartCustom(ChartEntry[] entries)
+        {
+            return new BarChart
+            {
+                Entries = entries,
+                IsAnimated = true,              
+                LabelTextSize = 35,            
+                MaxValue = 100,
+                MinValue = 0,
+                Margin = 50,
+                LabelOrientation = Orientation.Horizontal,
+                ValueLabelOrientation = Orientation.Horizontal,
+                BackgroundColor = SKColor.Parse("#333850"),
+                LabelColor = SKColor.Parse("#FFFFFF"),
+                Typeface = SKTypeface.Default,
+            };
+        }
+    }
+}
+
