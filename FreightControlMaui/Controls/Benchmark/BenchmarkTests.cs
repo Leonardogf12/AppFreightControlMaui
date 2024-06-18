3c832c1bbdb890062508e3bf187943eb8c6fccc8
leonardofazolo <leonardogf.contato@gmail.com>
2024-06-18 16:01:18 -0300

    Add new Folder and class
    Add Benchmark folder
    Add BenchmarkTest class

---
+++ FreightControlMaui/Controls/Benchmark/BenchmarkTests.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+
+namespace FreightControlMaui.Controls.Benchmark
+{
+    public static class BenchmarkTests
+    {
+        public static void StartStopWatch(Stopwatch stopWatch) => stopWatch.Start();
+
+        public static void StopWatchResult(Stopwatch stopWatch)
+        {
+            stopWatch.Stop();
+
+            TimeSpan ts = stopWatch.Elapsed;
+
+            Console.WriteLine($"*Runtime: {ts.Milliseconds}");
+        }
+    }
+
+}
+
