0154dc7d77516b27464ba6bf33240df52d201d31
leonardofazolo <leonardogf.contato@gmail.com>
2024-06-18 16:05:03 -0300

    Add new Folder and Class
    Add ControlCheckers Folder
    Add CheckTheEntrys Class

---
+++ FreightControlMaui/Controls/ControlCheckers/CheckTheEntrys.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.RegularExpressions;
+
+namespace FreightControlMaui.Controls.ControlCheckers
+{
+    public static class CheckTheEntrys
+    {
+        public const string patternKilometer = @"^[1-9][0-9]*(\.[0-9]{1,2})?$";
+        public const string patternMoney = @"^\d+(\.\d{1,2})?$";
+        public const string patternLiters = @"^[0-9]{1,4}$";
+
+        public static bool IsValidEntry(string input, string pattern)
+        {
+            return Regex.IsMatch(input, pattern);
+        }
+    }
+}
+
