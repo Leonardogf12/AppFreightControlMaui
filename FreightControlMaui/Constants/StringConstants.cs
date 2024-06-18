1f84b7d538afa143fbe2f4ad70c58b5e7cb85a38
leonardofazolo <leonardogf.contato@gmail.com>
2024-06-18 15:58:20 -0300

    Add new Folder and Class
    Add Constants Folder
    Add StringConstants Class

---
+++ FreightControlMaui/Constants/StringConstants.cs
@@ -0,0 +1,20 @@
+﻿namespace FreightControlMaui.Constants
+{
+    public static class StringConstants
+    {
+        public const string Excluir = "Excluir";
+        public const string ExcluirTudo = "Excluir Tudo";
+        public const string Exportar = "Exportar";
+        public const string Filtrar = "Filtrar";
+        public const string Cancelar = "Cancelar";
+        public const string urlDataIbgeService = "https://servicodados.ibge.gov.br/api/v1/localidades/estados/";
+        public const string DatabaseName = "confretedata.db3";
+        public const string webApiFirebaseAuthKey = "AIzaSyCCPH8VnM3b4Tu_8KBVE7rAPHHafhD33k0";
+        public const string authDomainFirebaseAddress = "com.companyname.freight.control.maui";
+        public const string firebaseAuthTokenKey = "FirebaseAuthToken";
+        public const string firebaseUserLocalIdKey = "firebaseUserLocalId";
+        public const string Usuario = "Usuário";
+        public const string IsBusy = "IsBusy";
+    }
+}
+
