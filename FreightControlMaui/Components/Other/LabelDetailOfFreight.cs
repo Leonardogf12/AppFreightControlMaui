fa239a49ff9682a94d85dc522222dc7a0f797dca
leonardofazolo <leonardogf.contato@gmail.com>
2024-06-18 15:40:08 -0300

    Add new Folders and Classes
    Add Folder Controls/Resources
    Add Folder Components/Other
    Add Class LabelDetailOfFreight
    Add Class ControlResources

---
+++ FreightControlMaui/Components/Other/LabelDetailOfFreight.cs
@@ -0,0 +1,42 @@
+﻿using FreightControlMaui.Controls.Resources;
+
+namespace FreightControlMaui.Components.Other
+{
+    public class LabelDetailOfFreight : StackLayout
+    {
+        public Label ContentLabel { get; set; }
+
+        public LabelDetailOfFreight(string title)
+        {
+            var stack = new StackLayout
+            {
+                Orientation = StackOrientation.Horizontal,
+                Spacing = 10
+            };
+
+            var titleLabel = new Label
+            {
+                Text = title,
+                TextColor = ControlResources.GetResource<Color>("PrimaryDark"),
+                FontFamily = "MontserratSemiBold",
+                FontSize = 16,
+                VerticalOptions = LayoutOptions.Center,
+            };
+
+            ContentLabel = new Label
+            {
+                TextColor = ControlResources.GetResource<Color>("PrimaryDark"),
+                FontFamily = "MontserratRegular",
+                FontSize = 16,
+                VerticalOptions = LayoutOptions.Center
+            };
+
+            stack.Children.Add(titleLabel);
+            stack.Children.Add(ContentLabel);
+
+            Children.Add(stack);
+        }
+    }
+
+}
+
---
+++ FreightControlMaui/Controls/Resources/ControlResources.cs
@@ -0,0 +1,16 @@
+﻿namespace FreightControlMaui.Controls.Resources
+{
+    public static class ControlResources
+	{
+        public static T? GetResource<T>(string name)
+        {
+            if (App.Current.Resources.TryGetValue(name, out var resourceValue) && resourceValue is T typedResource)
+            {
+                return typedResource;
+            }
+
+            return default;
+        }
+    }
+}
+
