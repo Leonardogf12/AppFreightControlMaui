bd62ca3b34247684c9b8d431c1c56d6f3c9a16de
leonardofazolo <leonardogf.contato@gmail.com>
2024-06-18 15:43:30 -0300

    Add Folder Popups
    Add Folder Popup
    Add class PopupLoadingView

---
+++ FreightControlMaui/Components/Popups/PopupLoadingView.cs
@@ -0,0 +1,42 @@
+﻿using CommunityToolkit.Maui.Views;
+using SkiaSharp.Extended.UI.Controls;
+
+namespace FreightControlMaui.Components.Popups
+{
+    public class PopupLoadingView : Popup
+    {
+        public bool IsOpen;
+
+        public PopupLoadingView()
+        {
+            Color = Colors.Transparent;
+            CanBeDismissedByTappingOutsideOfPopup = false;
+
+            Content = new VerticalStackLayout
+            {
+                BackgroundColor = Colors.Transparent,
+                VerticalOptions = LayoutOptions.Center,
+                HorizontalOptions = LayoutOptions.Center,
+                Children = {
+                    CreateLoadingComponent()
+                }
+            };
+        }
+
+        private static SKLottieView CreateLoadingComponent()
+        {
+            var load = new SKLottieView
+            {
+                Source = (SKLottieImageSource)SKLottieImageSource.FromFile("loading_truck.json"),
+                HeightRequest = 120,
+                WidthRequest = 120,
+                BackgroundColor = Colors.Transparent,
+                RepeatCount = -1,
+            };
+
+            return load;
+        }
+    }
+
+}
+
